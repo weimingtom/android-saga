cf7dfc709c69e8972821419a560ce30f7661c8f4
sagaAndroid <sagaAndroid@gmail.com>
2011-01-29 13:16:19 +0800

    2011/1/29 ???????????????????8??????ImageView??

--- gen/com/androidsaga/R.java
+++ gen/com/androidsaga/R.java
@@ -10,20 +10,38 @@ package com.androidsaga;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int bk_white=0x7f040000;
+    }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int bathbtn=0x7f020000;
+        public static final int exercisebtn=0x7f020001;
+        public static final int feedbtn=0x7f020002;
+        public static final int hospitalbtn=0x7f020003;
+        public static final int icon=0x7f020004;
+        public static final int presentbtn=0x7f020005;
+        public static final int qabtn=0x7f020006;
+        public static final int sleepbtn=0x7f020007;
     }
     public static final class id {
-        public static final int ImageView01=0x7f050001;
-        public static final int RelativeLayout01=0x7f050000;
-        public static final int TextView01=0x7f050002;
+        public static final int BathButton=0x7f060004;
+        public static final int ExerciseButton=0x7f060008;
+        public static final int FeedButton=0x7f060009;
+        public static final int HomeButton=0x7f060003;
+        public static final int HospitalButton=0x7f060007;
+        public static final int LinearLayout01=0x7f060000;
+        public static final int LinearLayout02=0x7f060002;
+        public static final int MainView=0x7f060001;
+        public static final int PresentButton=0x7f06000a;
+        public static final int QAButton=0x7f060006;
+        public static final int SleepButton=0x7f060005;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
-        public static final int testString=0x7f040002;
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
+        public static final int testString=0x7f050002;
     }
 }
--- src/com/androidsaga/SagaActivity.java
+++ src/com/androidsaga/SagaActivity.java
@@ -1,25 +1,103 @@
 package com.androidsaga;
 
 import android.app.Activity;
+import android.graphics.*;
 import android.os.Bundle;
-import android.view.Display;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View.*;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
 import android.widget.*;
 
 public class SagaActivity extends Activity {
-    /** Called when the activity is first created. */
+    /** Called when the activity is first created. **/
+	private ImageView MainView;
+	private ImageView HomeBtn;
+	private ImageView BathBtn;
+	private ImageView FeedBtn;
+	private ImageView ExerciseBtn;
+	private ImageView QABtn;
+	private ImageView SleepBtn;
+	private ImageView HospitalBtn;
+	private ImageView PresentBtn;
+	
+	private Integer width;
+	private Integer height;
+	private Bitmap MainImage;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature( Window.FEATURE_NO_TITLE );
+        
         setContentView(R.layout.main);
         
-        Display display = this.getWindowManager().getDefaultDisplay();
-        int height = display.getHeight();
+        MainView = (ImageView)findViewById(R.id.MainView);
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        width=dm.widthPixels;         
+               
+        height = dm.heightPixels - 25 - 
+        	BitmapFactory.decodeResource(this.getResources(), R.drawable.icon).getHeight();
         
-        ImageView mainImage = (ImageView)findViewById(R.id.ImageView01);
-        mainImage.setMinimumHeight(4*height/5);
+        //set main view size according to resolution        
+		MainImage = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        MainView.setImageBitmap(MainImage);        
         
-        TextView toShow = (TextView)findViewById(R.id.TextView01);
-        toShow.setHeight(height/5);
-        toShow.setText(getResources().getString(R.string.testString));
+        HomeBtn = (ImageView)findViewById(R.id.HomeButton);
+        BathBtn = (ImageView)findViewById(R.id.BathButton);
+        FeedBtn = (ImageView)findViewById(R.id.FeedButton);        
+        ExerciseBtn = (ImageView)findViewById(R.id.ExerciseButton);
+        QABtn = (ImageView)findViewById(R.id.QAButton);
+        SleepBtn = (ImageView)findViewById(R.id.SleepButton);
+        HospitalBtn = (ImageView)findViewById(R.id.HospitalButton);
+        PresentBtn = (ImageView)findViewById(R.id.PresentButton);
+        
+        //add button image    
+        HomeBtn.setImageResource(R.drawable.icon);   
+        BathBtn.setImageResource(R.drawable.bathbtn);        
+        FeedBtn.setImageResource(R.drawable.feedbtn);    
+        ExerciseBtn.setImageResource(R.drawable.exercisebtn);
+        QABtn.setImageResource(R.drawable.qabtn);
+        SleepBtn.setImageResource(R.drawable.sleepbtn);
+        HospitalBtn.setImageResource(R.drawable.hospitalbtn);
+        PresentBtn.setImageResource(R.drawable.presentbtn);        
     }
+    
+    public void clickHome(View target)
+    {
+    	Alert.showAlert("Saga", "Home", this);
+    }    
+    
+    public void clickBath(View target)
+    {
+    	Alert.showAlert("Saga", "Bath", this);
+    }   
+    
+    public void clickHospital(View target)
+    {
+    	Alert.showAlert("Saga", "Hospital", this);
+    }   
+    
+    public void clickFeed(View target)
+    {
+    	Alert.showAlert("Saga", "Feed", this);
+    }   
+    
+    public void clickPresent(View target)
+    {
+    	Alert.showAlert("Saga", "Present", this);
+    }   
+    
+    public void clickExercise(View target)
+    {
+    	Alert.showAlert("Saga", "Exercise", this);
+    }  
+    
+    public void clickQA(View target)
+    {
+    	Alert.showAlert("Saga", "Answer Question", this);
+    }   
 }
